61d97ea5bf41e1762b805669e251906661ebb4d3
Vlatko Kosturjak <kost@linux.hr>
2013-03-18 16:18:17 +0100

    Initial Cisco configuration parser by magnum
    Source:
    http://www.openwall.com/lists/john-dev/2013/03/17/28`

--- run/cisco2john.pl
+++ run/cisco2john.pl
@@ -1,13 +1,28 @@
 #!/usr/bin/perl
 
-# Basic Cisco type 4 - password encoder / decoder by Kost and Dhiru
+# Basic Cisco type 4 - password encoder / decoder by Kost, Dhiru
+# and magnum
 #
-# Usage: cisco2john.pl <Cisco type-4 hashes>
+# Usage Examples:
 #
-# Example:
+#   $ ./cisco2john.pl cisco.config >cisco.in
+#   #!comment: Found type 7 passwords:
+#   companysecret
+#   test
 #
-# $ cisco2john.pl LcV6aBcc/53FoCJjXQMd7rBUDEpeevrK8V5jQVoJEhU
-# 5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8
+# (because of that output, we re-run it and save stderr to its own file)
+#   $ ./cisco2john.pl cisco.conf >cisco.in 2>cisco.seed
+#
+#   $ cat cisco.in
+#   enable_secret_level_2:5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8
+#   enable_secret:$1$4C5N$JCdhRhHmlH4kdmLz.vsyq0
+#
+#   $ ./john -wo:cisco.seed -rules cisco.in
+#
+# Credits:
+#
+# magnum : Change cisco2john.pl so it reads a Cisco config file and outputs
+# any clear-text or deobfuscated passwords, and outputs hashes in JtR format.
 #
 # Base64 custom encoder / decoder taken from VOMS::Lite::Base64
 # This module was originally designed for the JISC funded SARoNGS project at developed at
@@ -25,7 +40,9 @@
 
 use strict;
 
-die "Usage: $0 <Cisco type-4 hashes>\n" if ($#ARGV lt 0);
+die "Usage: $0 [cisco config file] [...]\n" if ($ARGV[0] =~ /-h/);
+
+my $seedNotice = 1;
 
 my %Alphabets = ( VOMS => "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789[]",
                   RFC3548 => "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
@@ -78,11 +95,76 @@ sub Decode {
   return $data;
 }
 
-my $hash;
+my %uniq;
+sub unique
+{
+    my ($input) = @_;
+    return !$uniq{$input}++;
+}
 
-foreach $hash (@ARGV) {
+# Credits for original code and description hobbit@avian.org,
+# SPHiXe, .mudge et al. and for John Bashinski
+# for Cisco IOS password encryption facts.
+#
+# Use of this code for any malicious or illegal purposes is strictly prohibited!
+#
+sub deobfuscate
+{
+    my ($ep) = @_;
+    my @xlat = ( 0x64, 0x73, 0x66, 0x64, 0x3b, 0x6b, 0x66, 0x6f, 0x41,
+		 0x2c, 0x2e, 0x69, 0x79, 0x65, 0x77, 0x72, 0x6b, 0x6c,
+		 0x64, 0x4a, 0x4b, 0x44, 0x48, 0x53 , 0x55, 0x42 );
+    my $dp = "";
+    my ($s, $e) = ($2 =~ /^(..)(.+)/o);
+    for (my $i = 0; $i < length($e); $i+=2) {
+	$dp .= sprintf "%c",hex(substr($e,$i,2))^$xlat[$s++];
+    }
+    return $dp;
+}
 
-    my $binhash = Decode($hash, 'CISCO');
+sub notice
+{
+    if ($seedNotice) {
+	$seedNotice = 0;
+	print STDERR "#!comment: Found recoverable or clear-text passwords:\n";
+    }
+}
 
-    print join("", map { sprintf("%02x", ord($_)) } split(//, join("", $binhash))) . "\n";
+foreach (<>) {
+    s/[\r\n]//g;
+    if (/(password|md5|secret)\s+0\s/) {
+	notice();
+	s/\s+privilege\s+[0-9]+\s*$//;
+	s/[ :]+/_/g;
+	m/^.{1,}_0_(.*)/;
+	if (unique($1)) {
+	    print STDERR $1, "\n";
+	}
+    } elsif (/(password|md5)\s+7\s+([\da-f]+)/io) {
+	notice();
+	my $pw = deobfuscate($1);
+	if (unique($pw)) {
+	    print STDERR $pw, "\n";
+	}
+    } elsif (m/(\$1\$[\$\.\/0-9A-Za-z]{27,31})/) {
+	my $hash = $1;
+	s/[ :]+/_/g;
+	m/^(.{1,})_5_\$1\$.*/;
+	print $1, ":", $hash, "\n";
+    } elsif (m/([\$\.\/0-9A-Za-z]{43})/) {
+	my $hash = $1;
+	s/[ :]+/_/g;
+	m/^(.{1,})_4_[\$\.\/0-9A-Za-z]{43}/;
+	print $1, ":";
+	my $binhash = Decode($hash, 'CISCO');
+	print join("", map { sprintf("%02x", ord($_)) } split(//, join("", $binhash))), "\n";
+    } elsif (/(password|md5|secret)\s+[^045]/) {
+	notice();
+	s/\s+privilege\s+[0-9]+\s*$//;
+	s/[ :]+/_/g;
+	m/^((?:.*)(?:password|md5|secret))_(.*)/;
+	if (unique($2)) {
+	    print STDERR $2, "\n";
+	}
+    }
 }
